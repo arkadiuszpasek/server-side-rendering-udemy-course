a3129a69438af7e05b19aa0bdba4a29315461a27
Arkadiusz Pasek <thevik444@gmail.com>
2020-06-15 13:52:07 +0200

    Fetching users

---
+++ src/client/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <div>
+        Hello
+        <button onClick={() => console.log('Hello')}>Press me</button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
